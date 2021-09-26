e9db57b18a2f877a7514579fad3180a98a4d962f
Rahul Meghani <rahulmeghani36@gmail.com>
2021-09-25 22:05:29 -0400

    Added view comments

--- src/modules/components/DataInfo.js
+++ src/modules/components/DataInfo.js
@@ -6,6 +6,11 @@ import Button from '@mui/material/Button';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import CommentIcon from '@mui/icons-material/Comment';
 import TextField from '@mui/material/TextField';
+import { Divider, Avatar, Grid } from "@material-ui/core";
+
+
+const imgLink =
+  "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -35,7 +40,47 @@ export default function DataInfo() {
          <Item><Button variant="outlined"><CommentIcon/>Click to Add Comment</Button></Item>
          </Stack>
 
-         
+         <div style={{ padding: 14 }}>
+      <h1>Comments</h1>
+      <Paper style={{ padding: "40px 20px" }}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar alt="Remy Sharp" src={imgLink} />
+          </Grid>
+          <Grid justifyContent="left" item xs zeroMinWidth>
+            <h4 style={{ margin: 0, textAlign: "left" }}>Michel Michel</h4>
+            <p style={{ textAlign: "left" }}>
+                This is the test comment{" "}
+            </p>
+            <p style={{ textAlign: "left", color: "gray" }}>
+              posted 1 minute ago
+            </p>
+          </Grid>
+        </Grid>
+        {/* <Divider variant="fullWidth" style={{ margin: "10px 0" }} /> */}
+              </Paper>
+    
+
+    
+      <Paper style={{ padding: "40px 20px" }}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar alt="Remy Sharp" src={imgLink} />
+          </Grid>
+          <Grid justifyContent="left" item xs zeroMinWidth>
+            <h4 style={{ margin: 0, textAlign: "left" }}>Michel Michel</h4>
+            <p style={{ textAlign: "left" }}>
+                This is the test comment{" "}
+            </p>
+            <p style={{ textAlign: "left", color: "gray" }}>
+              posted 1 minute ago
+            </p>
+          </Grid>
+        </Grid>
+        {/* <Divider variant="fullWidth" style={{ margin: "10px 0" }} /> */}
+              </Paper>
+    </div> 
     </div>
+    
   );
 }
--- src/modules/views/AppAppBar.js
+++ src/modules/views/AppAppBar.js
@@ -5,6 +5,7 @@ import { Link as Direct} from 'react-router-dom';
 import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
 
+
 const rightLink = {
   fontSize: 16,
   color: 'common.white',
@@ -29,6 +30,7 @@ function AppAppBar() {
           </Direct>
 
           <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+          {}
           <Direct to="/sign-in" style={ {textDecoration: 'none', color:'white'} }>
             <Link
               color="inherit"
